f25a3eaa101640f755f1875c97fb8298afb5b97f
Arun <arunkushwahaayan@gmail.com>
2023-12-08 13:53:28 +0530

    waiterLife

--- 3-hour-project/src/Component/ExpenseForm.js
+++ 3-hour-project/src/Component/ExpenseForm.js
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-  const [enteredId, setEnteredId] = useState("");
-  const [enteredPrice, setEnteredPrice] = useState("");
-  const [enteredProduct, setEnteredProduct] = useState("");
-  const [selectCategary, setSelectCategary] = useState("");
+  const [enteredQuniqueId, setQniqueId] = useState("");
+  const [choosePrice, setChoosePrice] = useState("");
+  const [chooseDish, setChooseDish] = useState("");
+  const [chooseTable, setChooseTable] = useState("");
   const [allDetails, setAllDetails] = useState([]);
   const idInputHandler = (event) => {
-    setEnteredId(event.target.value);
+    setQniqueId(event.target.value);
   };
   const priceInputHandler = (event) => {
-    setEnteredPrice(event.target.value);
+    setChoosePrice(event.target.value);
   };
   const productInputHandler = (event) => {
-    setEnteredProduct(event.target.value);
+    setChooseDish(event.target.value);
   };
   const categaryHandler = (event) => {
-    setSelectCategary(event.target.value);
+    setChooseTable(event.target.value);
   };
   const formSubmit = (event) => {
     event.preventDefault();
-    if (enteredId && enteredPrice && enteredProduct && selectCategary) {
+    if (enteredQuniqueId && choosePrice && chooseDish && chooseTable) {
       const newEntry = {
         id: Math.random(),
-        enteredId: enteredId,
-        enteredPrice: enteredPrice,
-        enteredProduct: enteredProduct,
-        selectCategary: selectCategary,
+        enteredQuniqueId: enteredQuniqueId,
+        choosePrice: choosePrice,
+        chooseDish: chooseDish,
+        chooseTable: chooseTable,
       };
       setAllDetails([...allDetails, newEntry]);
     } else {
@@ -49,20 +49,19 @@ const ExpenseForm = () => {
         <label htmlFor="chooseCategary">Choose Categary:</label>
         <select name="product" id="catagary" onChange={categaryHandler}>
           <br />
-          <option>Food</option>
-          <option>Electronic</option>
-          <option>SkinCare</option>
+          <option>Table1</option>
+          <option>Table2</option>
+          <option>Table3</option>
         </select>
-        <button type="submit">Add Product</button>
+        <button type="submit">Add to Bill</button>
       </form>
       <div></div>
       {allDetails.map((curElem) => {
         return (
           <div className="list" id={curElem.id}>
             <pre>
-              ID: {curElem.enteredId} Price: {curElem.enteredPrice}
-              Product: {curElem.enteredProduct} categary:{" "}
-              {curElem.selectCategary}
+              OrderID: {curElem.enteredQuniqueId} Price: {curElem.choosePrice}{" "}
+              Dish: {curElem.chooseDish} Table: {curElem.chooseTable}
             </pre>
           </div>
         );
